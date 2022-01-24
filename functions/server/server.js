6c90f01128ffccfe72ca5fa2925d7d004ad8bbdc
Tifiloow <theophilejeromerocher44@gmail.com>
2022-01-24 14:53:49 +0100

    ..

--- functions/server/server.js
+++ functions/server/server.js
@@ -11,11 +11,8 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/test', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
