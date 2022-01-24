417b24f502bfa83de0323734a285096665cf4381
Tifiloow <theophilejeromerocher44@gmail.com>
2022-01-24 15:10:25 +0100

    ..

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const app = require('./functions/server/server');
+const app = require('./functions/server/server.js');
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
